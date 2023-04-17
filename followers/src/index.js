2c98e0bbbcc62e80e69ea0821716a829ff898ad6
Arthur Korostelev <korostelev.art.ua@gmail.com>
2023-04-17 14:51:53 +0300

    fix

--- followers/src/index.js
+++ followers/src/index.js
@@ -12,7 +12,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="/Followers/">
+      <BrowserRouter basename="/Followers">
         <App />
       </BrowserRouter>
     </Provider>
